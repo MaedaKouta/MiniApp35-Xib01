e34591b1081596972d4030200baac78ee5abf623
Kota <maedakoutaa@gmail.com>
2022-04-04 00:01:40 +0900

    commit

---
+++ MiniApp35-Xib01/GafaTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GafaTableViewCell.swift
+//  MiniApp35-Xib01
+//
+//  Created by 前田航汰 on 2022/04/03.
+//
+
+import UIKit
+
+class GafaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var gafaLabel: UILabel!
+
+}
--- MiniApp35-Xib01/ViewController.swift
+++ MiniApp35-Xib01/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    private let gafaList = ["Google","Amazon","FaceBook","Apple"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "GafaTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        gafaList.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GafaTableViewCell
+        cell.gafaLabel?.text = gafaList[indexPath.row]
+        return cell
+    }
 
 }
 
